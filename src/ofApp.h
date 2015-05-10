90cacca72a79ee9f418174471e2c46f7f91a714f
ACE <andrew.emil@docurated.com>
2015-05-10 12:45:49 -0400

    Initial commit for visualizer project

---
+++ src/ofApp.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxFft.h"
+
+class ofApp : public ofBaseApp{
+	
+	public:
+		
+		void setup();
+		void update();
+		void draw();
+		
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+        void plot(vector<float>& buffer, float scale, float offset);
+				
+		void audioIn(float * input, int bufferSize, int nChannels); 
+	
+		vector <float> left;
+		vector <float> right;
+		vector <float> volHistory;
+        vector <float> drawBins, middleBins, audioBins;
+		
+		int 	bufferCounter;
+		int 	drawCounter;
+        int     plotHeight;
+        int     bufferSize;
+		
+		float smoothedVol;
+		float scaledVol;
+    
+        ofxFft* fft;
+		
+    ofMutex soundMutex;
+		ofSoundStream soundStream;
+};
